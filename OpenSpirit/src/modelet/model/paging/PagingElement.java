ee620dc2083c4931468be149c0d84df619d45aa0
matt.jiang@gmail.com <matt.jiang@gmail.com@25d31630-a1c7-11dd-a648-ff032ea17e63>
2009-04-15 13:55:26 +0000

    Enhancement: If required page number exceed actual total pages, then return page 1 results instead.

---
+++ OpenSpirit/src/modelet/model/paging/PagingElement.java
@@ -0,0 +1,33 @@
+package modelet.model.paging;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Component("pagingElement")
+public class PagingElement {
+
+  private Integer targetPage = new Integer(1);
+  
+  @Autowired
+  private Integer rowsPerPage;
+
+  
+  public Integer getTargetPage() {
+    return targetPage;
+  }
+  
+  public void setTargetPage(Integer targetPage) {
+    this.targetPage = targetPage;
+  }
+  
+  public Integer getRowsPerPage() {
+    return rowsPerPage;
+  }
+  
+  public void setRowsPerPage(Integer rowsPerPage) {
+    this.rowsPerPage = rowsPerPage;
+  }
+  
+  
+}
