6fac4f9141c28529254fe38814cfa930c0e13fda
matt.jiang@gmail.com <matt.jiang@gmail.com@25d31630-a1c7-11dd-a648-ff032ea17e63>
2009-05-14 03:20:47 +0000

    bug fix: Because MySQL JDBC driver didn't return alias column name by using getColumnName(), so changed to use getColumnLabel() instead.
    Misc:
    1. Add JavaDoc to Entity interface
    2. Add extra method definition to Model interface

--- trunk/OpenSpirit/src/modelet/model/Model.java
+++ trunk/OpenSpirit/src/modelet/model/Model.java
@@ -1,15 +1,25 @@
 package modelet.model;
 
 import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
 
 import modelet.entity.Entity;
+import modelet.model.paging.PageContainer;
+import modelet.model.paging.PagingElement;
 
 
 public interface Model {
 
   public <E extends Entity> List<E> find(String sql, Object[] params, Class<E> clazz);
+  public List<Map<String, Object>> find(String sql, Object[] params);
+  
+  public PageContainer<SortedMap<String, Object>> findWithPaging(String sql, Object[] params, final PagingElement pagingElement) throws ModelException;
+  public <E extends Entity> PageContainer<E> findWithPaging(String sql, Object[] params, final Class<E> clazz, final PagingElement pagingElement) throws ModelException;
+  
+  public <E extends Entity> E findOne(String sql, Object[] params, Class<E> clazz);
+  
   public void save(Entity entity) throws ModelException;
   public <E extends Entity> void save(List<E> entities) throws ModelException;
-//  public <E extends Entity> E load(Class<E> clazz, Object id) throws ModelException;
   
 }
