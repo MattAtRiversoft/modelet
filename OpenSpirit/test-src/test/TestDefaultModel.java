8e219f0d24081429aa1f4d47f92dfb3dd186156e
MattAtRiversoft <matt.jiang@gmail.com>
2016-06-01 15:31:48 +0800

    enhance test case to deal with Java Enum object

--- OpenSpirit/test-src/test/TestDefaultModel.java
+++ OpenSpirit/test-src/test/TestDefaultModel.java
@@ -2,25 +2,56 @@ package test;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.Test;
 
+import modelet.entity.TxnMode;
 import modelet.model.DefaultModel;
 import test.entity.Book;
 import util.ApplicationContextHelper;
 
 public class TestDefaultModel {
 
+  public static void main(String[] args) {
+   
+    TestDefaultModel me = new TestDefaultModel();
+    me.testPersist();
+  }
+  
   @Test
   public void testPersist() {
     
     Book book = new Book();
-    book.setBookName("賈'伯'斯'傳");
+    book.setBookName("Java學習筆記 第四版");
     book.setCreateDate(new Date());
-    book.setPrice(new BigDecimal("100"));
+    book.setPrice(new BigDecimal("150"));
     book.setAEnum(AEnum.A);
     
     DefaultModel model = ApplicationContextHelper.getBean("defaultModel");
     model.save(book);
+    
+    book.setAEnum(AEnum.B);
+    
+    model.save(book);
+    
+    book.setTxnMode(TxnMode.DELETE);
+    model.save(book);
+    
+    
+    Book myBook = model.getEntityById(4L, "book", Book.class);
+    System.out.println(myBook.getBookName());
+    
+    Object[] params = {1L};
+    List<Book> books = model.find("select * from book where id > ? ", params, Book.class);
+    for (Book abook : books) {
+      System.out.println(abook.getBookName());
+    }
+    
+    List<Map<String, Object>> rows = model.find("select * from book where id > ? ", params);
+    for (Map<String, Object> row : rows) {
+      System.out.println(row.get("bookName") + "," + row.get("createDate"));
+    }
   }
 }
